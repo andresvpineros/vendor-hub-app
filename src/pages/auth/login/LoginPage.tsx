c05221d87a112da2b549cca68668931a309a58bf
Brandon Vargas <andres.vpineros@gmail.com>
2025-06-03 11:27:48 -0500

    Refactor LoginPage to include a Popover with demo credentials and improve form structure

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,10 +1,23 @@
-import { Button, Checkbox, Form, Input, Flex, Spin, notification } from "antd";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Flex,
+  Spin,
+  notification,
+  Popover,
+  Typography,
+} from "antd";
 import type { FormProps } from "antd";
 import styles from "./LoginPage.module.css";
 import { formSchema } from "@/modules/auth/ui/schemas/login.schema";
 import { useNavigate } from "react-router-dom";
 import { UserSignsInUseCase } from "@/modules/auth/domain/use_cases/userSignsIn.usecase";
 import { useState } from "react";
+import { InfoCircleFilled } from "@ant-design/icons";
+
+const { Text } = Typography;
 
 type FieldType = {
   email?: string;
@@ -17,6 +30,19 @@ type FormItemType = {
   name: string;
   rules?: { required: boolean; message: string }[];
 };
+const title = (
+  <span>
+    <InfoCircleFilled style={{ color: "orange", marginRight: "5px" }} />
+    Credenciales de prueba
+  </span>
+);
+const content = (
+  <div>
+    <Text strong>Usuario:</Text> <Text copyable>test@vendorhub.com</Text>
+    <br />
+    <Text strong>Contraseña:</Text> <Text copyable>Test123!</Text>
+  </div>
+);
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -53,67 +79,71 @@ const LoginPage = () => {
   return (
     <div className={styles.container}>
       {contextHolder}
-      <div className={styles.formContainer}>
-        <div style={{ textAlign: "center" }}>
-          <img src="/img/logo-general.png" alt="" width={200} />
-        </div>
-        <div className={styles.infoContainer}>
-          <h1 className={styles.title}>Ingresar</h1>
-          <span style={{ color: "gray" }}>
-            No tienes una cuenta?{" "}
-            <b style={{ color: "#068fff" }}>Crea una nueva</b>
-          </span>
-        </div>
-        <Form
-          name="basic"
-          layout="vertical"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-          requiredMark="optional"
-        >
-          <Form.Item<FieldType>
-            label={
-              <span className={styles.inputLabel}>Correo electrónico</span>
-            }
-            name="email"
-            rules={formSchema.email as FormItemType["rules"]}
+      <Popover open placement="rightTop" title={title} content={content}>
+        <div className={styles.formContainer}>
+          <div style={{ textAlign: "center" }}>
+            <img src="/img/logo-general.png" alt="" width={200} />
+          </div>
+          <div className={styles.infoContainer}>
+            <h1 className={styles.title}>Ingresar</h1>
+            <span style={{ color: "gray" }}>
+              No tienes una cuenta?{" "}
+              <b style={{ color: "#068fff" }}>Crea una nueva</b>
+            </span>
+          </div>
+          <Form
+            name="basic"
+            layout="vertical"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
+            requiredMark="optional"
           >
-            <Input size="large" />
-          </Form.Item>
+            <Form.Item<FieldType>
+              label={
+                <span className={styles.inputLabel}>Correo electrónico</span>
+              }
+              name="email"
+              initialValue="test@vendorhub.com"
+              rules={formSchema.email as FormItemType["rules"]}
+            >
+              <Input size="large" />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label={<span className={styles.inputLabel}>Contraseña</span>}
-            name="password"
-            rules={formSchema.password as FormItemType["rules"]}
-          >
-            <Input.Password size="large" />
-          </Form.Item>
-          <Form.Item<FieldType>
-            name="remember"
-            valuePropName="checked"
-            label={null}
-          >
-            <Checkbox>Recuérdame</Checkbox>
-          </Form.Item>
-          <Form.Item label={null}>
-            <Flex gap="left">
-              <Spin spinning={loading} delay={100}>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className={styles.loginButton}
-                >
-                  Ingresar
-                </Button>
-              </Spin>
-            </Flex>
-          </Form.Item>
-          <span style={{ color: "#068fff", fontWeight: 600 }}>
-            Olvidaste tu contraseña?
-          </span>
-        </Form>
-      </div>
+            <Form.Item<FieldType>
+              label={<span className={styles.inputLabel}>Contraseña</span>}
+              name="password"
+              initialValue="Test123!"
+              rules={formSchema.password as FormItemType["rules"]}
+            >
+              <Input.Password size="large" />
+            </Form.Item>
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              label={null}
+            >
+              <Checkbox>Recuérdame</Checkbox>
+            </Form.Item>
+            <Form.Item label={null}>
+              <Flex gap="left">
+                <Spin spinning={loading} delay={100}>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className={styles.loginButton}
+                  >
+                    Ingresar
+                  </Button>
+                </Spin>
+              </Flex>
+            </Form.Item>
+            <span style={{ color: "#068fff", fontWeight: 600 }}>
+              Olvidaste tu contraseña?
+            </span>
+          </Form>
+        </div>
+      </Popover>
 
       <span className={styles.developmentInfo}>
         Developed by{" "}
